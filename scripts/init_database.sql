686f6813e1854305c26320b990f6558ddf3a6df3
apejovicc <101402195+apejovicc@users.noreply.github.com>
2026-04-16 08:51:30 +0200

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,22 @@
+ 
+use master;
+go
+
+if exists(select 1 from sys.databases where name = 'datawarehouse')
+begin
+	alter database datawarehouse set single_user with rollback immediate;
+	drop database datawarehouse
+ end; 
+ go
+
+create database  datawarehouse;
+go
+
+use datawarehouse;
+go
+
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
